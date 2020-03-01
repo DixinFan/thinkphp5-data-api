e4e640ae5070349e0779057dafd02aabf298d761
DixinFan <dixin.fan@outlook.com>
2020-03-01 13:15:30 +0800

    Finishing doLogin API.

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,20 +2,43 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Db;
+// use Swagger\Annotations as SWG;
 
 class Index extends Controller
 {
     public function index()
     {
-        $data = Db::name('think_data')->find();
+        $data = Db::name('users')->find();
+        // $swagger=\Swagger\scan(__DIR__);  
+        // $res=$swagger->saveAs('./swagger.json');  
         // $data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
-        return ['data'=>$data,'code'=>1,'message'=>'操作完成'];
-    }
-    public function tq($data='')
-    {
         //$data = Db::name('think_data')->find();
         // $data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
         return ['data'=>$data,'code'=>1,'message'=>'操作完成'];
+    }
+    public function doLogin($userName='',$password='')
+    {   
+        $has = db('users')->where('user_email', $userName)->find();
+        $data = '';
+        $code = -1;
+        $message = '';
+    	if(empty($has)){
+            $code = 401;
+            $message = '用户不存在';
+    	}else{
+            // if($has['user_pwd'] != md5($password)){
+            if($has['user_pwd'] != $password){
+                $code = 401;
+                $message = '用户密码错误';
+            }else{
+                $code = 200;
+                $message = '登录成功';
+            }
+        }
+    	// if($has['user_pwd'] != md5($param['user_pwd'])){
+    	// }
+        // $data =['userName'=>$userName,'password'=>$password];
+        return ['data'=>$data,'code'=>$code,'message'=>$message];
     }
     public function upload(){
         // 获取表单上传文件 例如上传了001.jpg
--- public/index.php
+++ public/index.php
@@ -10,7 +10,11 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-
+header('Access-Control-Allow-Origin:*');  
+// 响应类型  
+header('Access-Control-Allow-Methods:*');  
+// 响应头设置  
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
